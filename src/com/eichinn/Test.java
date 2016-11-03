a4fc3f8ddee0cdda6a6c2fccfdf858bb08ae0f36
EiChinn <eichinn77@gmail.com>
2016-11-03 23:02:59 +0800

    test

---
+++ src/com/eichinn/Test.java
@@ -0,0 +1,10 @@
+package com.eichinn;
+
+/**
+ * Created by chenrong on 2016/11/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Design Pattern!");
+    }
+}
